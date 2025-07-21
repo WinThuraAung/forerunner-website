2c848290cbcfd91de5fc484906066f2fb7f37fa7
Winthuraaung <Ronaldwu5@gmail.com>
2025-07-20 22:32:00 -0700

    added contactform

---
+++ forerunner-backend/index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = 5000;
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+app.post('/send-email', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER,
+        subject: `Contact Form Submission from ${name}`,
+        text: `Name: ${name}\nEmail: ${email}\n\n${message}`,
+    };
+
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ success: true, message: 'Email sent successfully!' });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json({ success: false, message: 'Failed to send email.' });
+    }
+
+
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+
+
+
+
